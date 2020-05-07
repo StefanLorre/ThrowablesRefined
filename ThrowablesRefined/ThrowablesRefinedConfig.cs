faab197cdec8567cc39270808e97240404028148
Stefan Lorre <Stefan.Lorre96@googlemail.com>
2020-05-07 04:30:05 +0200

    Add files via upload

---
+++ ThrowablesRefined/ThrowablesRefinedConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+
+namespace ThrowablesRefined
+{
+    public class ThrowablesRefinedConfig
+    {
+        public ThrowablesRefinedConfig()
+        {
+            XmlReaderSettings xmlReaderSettings = new XmlReaderSettings();
+            xmlReaderSettings.IgnoreComments = true;
+            using (XmlReader xmlReader = XmlReader.Create(ThrowablesRefinedConfig.FILE_NAME, xmlReaderSettings))
+            {
+                this.config.Load(xmlReader);
+            }
+        }
+        private static string FILE_NAME = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/ThrowablesRefinedConfig.xml";
+        public XmlDocument config = new XmlDocument();
+    }
+}
---
+++ ThrowablesRefined/TrowablesRefinedMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace ThrowablesRefined
+
+{
+    public class ThrowablesRefinedMain : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Harmony.DEBUG = true;
+            FileLog.Reset();
+            Harmony harmony = new Harmony("mod.throwablesrefined.luna");
+            try
+            {
+                harmony.PatchAll();
+            }
+            catch (Exception ex)
+            {
+                string str = "Error patching:\n";
+                string message = ex.Message;
+                string str2 = " \n\n";
+                Exception innerException = ex.InnerException;
+                FileLog.Log("ThrowablesRefined fucked up" + str + message + str2 + ((innerException != null) ? innerException.Message : null));
+            }
+
+            CustomStackAmount.initValues();
+            CustomMissileDamage.initValues();
+            CustomMissileSpeed.initValues();
+        }
+
+        public static ThrowablesRefinedConfig config = new ThrowablesRefinedConfig();
+    }
+}
+
+
